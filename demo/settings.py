6dd71814f89ca01fe22d7d2c70d5c5678a4a9c18
Marco Antonio Vazquez Alonso <mrc_ant_18@hotmail.com>
2014-09-19 20:48:39 -0500

    Update settings.py
    borrar

--- demo/settings.py
+++ demo/settings.py
@@ -161,5 +161,5 @@ LOGGING = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mmarco.antvaz@gmail.com'
-EMAIL_HOST_PASSWORD = 'm18a06v89agmail'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
